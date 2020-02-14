80727fff54e7a26ed71225f8edbf205c1d7ef7f6
Administrator <Administrator@AA-PC>
2020-02-14 19:16:19 +0530

    firstupdation

---
+++ maven_project_174/src/test/java/p1/ClassA.java
@@ -0,0 +1,36 @@
+package p1;
+
+import org.testng.annotations.Test;
+
+public class ClassA 
+{
+  @Test
+  public void tc001() 
+  {  
+	  System.out.println("tc001 executed");
+  }
+  
+  @Test
+  public void tc002() 
+  {  
+	  System.out.println("tc002 executed");
+  }
+  
+  @Test
+  public void tc003() 
+  {  
+	  System.out.println("tc003 executed");
+  }
+
+  @Test
+  public void login() 
+  {  
+	  System.out.println("login executed");
+  }
+
+  @Test
+  public void reg() 
+  {  
+	  System.out.println("reg executed");
+  }
+}
